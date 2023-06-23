d2dde75aa1c2e7d001049aa870469d6eeef92839
DebyGrey <demifashoro@gmail.com>
2023-06-23 09:49:37 +0100

    Adds reducers and actions

--- src/routes/PageNotFound.jsx
+++ src/routes/PageNotFound.jsx
@@ -1,6 +1,15 @@
-const PageNotFound = () => (
-  <div>
-    <p>Page Not found!</p>
-  </div>
-);
-export default PageNotFound;
+import { useRouteError } from "react-router-dom";
+
+export default function PageNotFound() {
+  const error = useRouteError();
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Something went wrong.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
