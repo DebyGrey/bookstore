9f2c2279de3619155c71ec3416d24e9d229e315e
DebyGrey <demifashoro@gmail.com>
2023-06-23 20:13:37 +0100

    styling

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 import Book from "./Book";
 import CreateABookForm from "./CreateABookForm";
 import { getBooks } from "../redux/books/booksSlice";
@@ -19,9 +20,11 @@ const BookList = () => {
   }, []);
 
   return (
-    <div>
+    <Section>
       {isLoading ? (
-        <div>Loading...</div>
+        <LoadingSpinner>
+          <Spinner />
+        </LoadingSpinner>
       ) : (
         <>
           {booksArray.map((book) => (
@@ -30,8 +33,35 @@ const BookList = () => {
           <CreateABookForm />
         </>
       )}
-    </div>
+    </Section>
   );
 };
 
 export default BookList;
+
+
+const Section = styled.section``;
+
+const LoadingSpinner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+`;
+
+const Spinner = styled.div`
+  width: 40px;
+  height: 40px;
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  animation: spin 2s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
--- src/components/CreateABookForm.jsx
+++ src/components/CreateABookForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postBook, getBooks } from "../redux/books/booksSlice";
+import styled from "styled-components";
 
 const CreateABookForm = () => {
   const dispatch = useDispatch();
@@ -21,27 +22,57 @@ const CreateABookForm = () => {
   };
 
   return (
-    <div className="form-wrapper">
-      <h1>ADD NEW BOOK</h1>
+    <Section className="form-wrapper">
+      <h1 className="title">ADD NEW BOOK</h1>
       <form className="book-form">
         <input
+          className="book-title"
           type="text"
           placeholder="Book title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
+          className="book-author"
           type="text"
           placeholder="Book author"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
-        <button className="add-book" type="submit" onClick={handleSubmit}>
+        <button className="add-book-btn" type="submit" onClick={handleSubmit}>
           ADD BOOK
         </button>
       </form>
-    </div>
+    </Section>
   );
 };
 
 export default CreateABookForm;
+
+const Section = styled.section`
+  border-top: 1px solid #e8e8e8;
+  margin: 2rem 6.25rem;
+  .title {
+    height: 1.5rem;
+    margin: 1.813rem 32.75rem 1.188rem 0;
+    font-size: 1.25rem;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.18px;
+    color: #888;
+  }
+  .book-form {
+    display: flex;
+    justify-content: space-between;
+    
+    .add-book-btn {
+      height: 2.813rem;
+      border-radius: 3px;
+      color: #fff;
+      background-color: #2e90f0;
+      width: 15.28%;
+    }
+  }
+`;
